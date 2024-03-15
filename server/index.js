b66df5286d485ba562f9bac047a02f15604d44d0
Kanchana224 <kkanchana427@gmail.com>
2024-03-15 11:18:55 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,34 +1,71 @@
+// Import required modules
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+
+// Create Express application
+const app = express();
+
+// Load environment variables
 require('dotenv').config();
 
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+// Set the port
+const PORT = process.env.PORT || 2000;
 
-const LoginRoute = require("./routes/LoginRoute");
-const RegisterRoute = require("./routes/RegisterRoute");
-const RecipeRoute = require("./routes/RecipeRoute");
-const ForgotPassword = require("./routes/forgotPassword");
-const Home = require("./controllers/controller");
-const verifyToken = require("./Middleware/middleware");
+// Generate secret key
+const secretKey = crypto.randomBytes(32).toString('hex');
+console.log('Generated Secret Key:', secretKey);
 
+// Set the SECRET environment variable
+process.env.SECRET = secretKey;
+
+// JWT token verification middleware
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to the database");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server Started on port ${process.env.PORT}`);
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server Started on port ${PORT}`);
     });
   })
   .catch((error) => {
     console.error("Error connecting to the database:", error);
   });
 
+// Routes
+const LoginRoute = require("./routes/LoginRoute");
+const RegisterRoute = require("./routes/RegisterRoute");
+const RecipeRoute = require("./routes/RecipeRoute");
+const ForgotPassword = require("./routes/forgotPassword");
+const Home = require("./controllers/controller");
+
 app.use("/auth", LoginRoute);
 app.use("/auth", RegisterRoute);
 app.use("/auth", RecipeRoute);
 app.use("/auth", ForgotPassword);
 
+// Home route with token verification middleware
 app.get("/", verifyToken, Home.Home);
+
+// Export the app
+module.exports = app;
