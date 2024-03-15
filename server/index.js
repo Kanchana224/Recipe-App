24c5d0682e95df64c85ea6f70a7ccd9c817089e6
Kanchana224 <kkanchana427@gmail.com>
2024-03-15 11:03:25 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,35 +1,34 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-dotenv.config();
-const router = express.Router();
+const mongoose = require("mongoose");
+
+const LoginRoute = require("./routes/LoginRoute");
+const RegisterRoute = require("./routes/RegisterRoute");
+const RecipeRoute = require("./routes/RecipeRoute");
+const ForgotPassword = require("./routes/forgotPassword");
+const Home = require("./controllers/controller");
+const verifyToken = require("./Middleware/middleware");
 
 app.use(express.json());
 app.use(cors());
 
-const config = require("./db/config");
-const Home = require("./controllers/controller");
-const LoginRoute = require("./routes/LoginRoute");
-const RegisterRoute = require("./routes/RegisterRoute");
-const verifyToken = require("./Middleware/middleware");
-const RecipeRoute = require("./routes/RecipeRoute");
-const ForgotPassword = require("./routes/forgotPassword");
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Started on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
 
 app.use("/auth", LoginRoute);
 app.use("/auth", RegisterRoute);
 app.use("/auth", RecipeRoute);
-app.use("/auth", router);
 app.use("/auth", ForgotPassword);
 
-router.get("/", verifyToken, Home.Home);
-
-module.exports = router;
-
-if (config) {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server Started on port ${process.env.PORT}`);
-  });
-}
+app.get("/", verifyToken, Home.Home);
